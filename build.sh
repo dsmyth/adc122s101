eb67caffc28ba5caabbdbc794234d6ae2bb16a5b
Don Smyth <don@snupi.com>
2013-03-05 16:56:18 -0800

    Update build.sh for the newer toolchain

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
-BB=$PWD/../Beaglebone-Buildroot
+BB=$HOME/Work/Beaglebone-Buildroot
 KERNEL=linux-master
 export KERNELDIR=${BB}/output/build/${KERNEL}
 
-export PATH=${PATH}:${BB}/output/host/usr/bin
+export PATH=${PATH}:${BB}/output/host/opt/ext-toolchain/bin:${BB}/output/host/usr/bin
 
 unset CFLAGS CPPFLAGS CXXFLAGS LDFLAGS
 
 export ARCH="arm"
 export CROSS_COMPILE="arm-linux-gnueabihf-"
 export CC="${CROSS_COMPILE}gcc"
+export CXX="${CROSS_COMPILE}g++"
 export LD="${CROSS_COMPILE}ld"
 export STRIP="${CROSS_COMPILE}strip"
-make $* BINDIR=/export/rootfs/root MODDIR=/export/rootfs/root
+make $* BINDIR=/export/rootfs/root MODDIR=/export/rootfs/lib/modules
